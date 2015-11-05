557683fc6d3fbe4f1e2206053979116ad5859f66
CaptainJuneBug <mikeblacknes@gmail.com>
2015-11-05 10:09:13 -0600

    Testing 2
    Removed Assignment File and changed form size

--- Pinata Game/Pinata Game/Form1.Designer.cs
+++ Pinata Game/Pinata Game/Form1.Designer.cs
@@ -32,9 +32,10 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(506, 445);
+            this.ClientSize = new System.Drawing.Size(1203, 473);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
