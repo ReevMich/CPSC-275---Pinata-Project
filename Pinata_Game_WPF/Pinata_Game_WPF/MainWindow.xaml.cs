26fcaa791bb668b3e6f16b5b9e6731868a0542bd
CaptainJuneBug <mikeblacknes@gmail.com>
2015-11-09 16:48:21 -0600

    Added WPF version of the game to the proect
    Check out the pinata class. Play around with the stuff in the main form
    and pinata class to get an understanding of how things are working.

---
+++ Pinata_Game_WPF/Pinata_Game_WPF/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Pinata_Game_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Pinata pinata;
+        private DispatcherTimer timer;
+
+        private Storyboard storyBoard;
+
+        public MainWindow()
+        {
+            StackPanel myPanel = new StackPanel();
+            myPanel.Margin = new Thickness(10);
+
+            InitializeComponent();
+            storyBoard = new Storyboard();
+            pinata = new Pinata(this);
+
+            timer = new DispatcherTimer();
+
+            //  DispatcherTimer setup
+            timer = new System.Windows.Threading.DispatcherTimer();
+            timer.Tick += new EventHandler(dispatcherTimer_Tick);
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 1);
+            timer.Start();
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            pinata.Draw();
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            // Pause Logic will go in here.
+            if (e.Key == Key.P)
+            {
+                Console.WriteLine("Pause");
+            }
+        }
+    }
+}
---
+++ Pinata_Game_WPF/Pinata_Game_WPF/Pinata.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Pinata_Game_WPF
+{
+    internal class Pinata
+    {
+        private Line myLine;
+        private Ellipse myEllipse;
+        private MainWindow parentWindow;
+        private bool goingRight = true;
+        private float i = 0;
+        private float incrementAngle = 1.5f;
+
+        public Pinata(MainWindow window)
+        {
+            this.parentWindow = window;
+            InitializeComponents();
+        }
+
+        private void InitializeComponents()
+        {
+            // Add a Line Element
+            myLine = new Line();
+            myLine.Stroke = System.Windows.Media.Brushes.LightSteelBlue;
+            myLine.X1 = parentWindow.Width / 2;
+            myLine.X2 = parentWindow.Width / 2;
+            myLine.Y1 = 0;
+            myLine.Y2 = myLine.Y1 + 100;
+            myLine.HorizontalAlignment = HorizontalAlignment.Left;
+            myLine.VerticalAlignment = VerticalAlignment.Center;
+            myLine.StrokeThickness = 2;
+
+            // Create a red Ellipse.
+            myEllipse = CreateEllipse(25, 25, myLine.X2, myLine.Y2);
+
+            // Create a SolidColorBrush with a red color to fill the
+            // Ellipse with.
+            SolidColorBrush mySolidColorBrush = new SolidColorBrush();
+
+            // Describes the brush's color using RGB values.
+            // Each value has a range of 0-255.
+            mySolidColorBrush.Color = Color.FromArgb(255, 0, 0, 0);
+
+            myEllipse.Fill = mySolidColorBrush;
+            myEllipse.StrokeThickness = 2;
+
+            // Set the width and height of the Ellipse.
+            myEllipse.Width = 25;
+            myEllipse.Height = 25;
+
+            double left = myLine.X2 - (parentWindow.Width / 2);
+            double top = myLine.Y2 - (parentWindow.Height / 2);
+
+            parentWindow.TheCanvas.Children.Add(myLine);
+            parentWindow.TheCanvas.Children.Add(myEllipse);
+        }
+
+        private Ellipse CreateEllipse(double width, double height, double desiredCenterX, double desiredCenterY)
+        {
+            Ellipse ellipse = new Ellipse { Width = width, Height = height };
+
+            double left = desiredCenterX - (width / 2);
+            double top = desiredCenterY - (height / 2);
+
+            ellipse.Margin = new Thickness(left, top, 0, 0);
+            return ellipse;
+        }
+
+        public void Draw()
+        {
+            if (goingRight)
+            {
+                myLine.RenderTransform = new RotateTransform(i -= incrementAngle, myLine.X1, myLine.Y1);
+            }
+            else
+            {
+                myLine.RenderTransform = new RotateTransform(i += incrementAngle, myLine.X1, myLine.Y1);
+            }
+
+            RotateTransform rotation = myLine.RenderTransform as RotateTransform;
+            Point p1 = rotation.Transform(new Point(myLine.X1, myLine.Y1));
+            Point p2 = rotation.Transform(new Point(myLine.X2, myLine.Y2));
+
+            myEllipse.Margin = new Thickness(p2.X - (myEllipse.Width / 2), p2.Y - (myEllipse.Height / 2), 0, 0);
+
+            if (i == -57)
+            {
+                goingRight = false;
+                Console.WriteLine("Line Angle : right");
+            }
+            else if (i == 57)
+            {
+                goingRight = true;
+                Console.WriteLine("Line Angle : left");
+            }
+        }
+    }
+}
