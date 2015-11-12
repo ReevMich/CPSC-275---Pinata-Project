ab2e56bd722d8692c07bcee43a694c4b51e93494
CaptainJuneBug <mikeblacknes@gmail.com>
2015-11-12 00:39:03 -0600

    Main Window Fix

--- Pinata_Game_WPF/Pinata_Game_WPF/MainWindow.xaml.cs
+++ Pinata_Game_WPF/Pinata_Game_WPF/MainWindow.xaml.cs
@@ -47,7 +47,7 @@ namespace Pinata_Game_WPF
             // and our bat objects.
             if (!isPaused)
             {
-                 bat.Draw();
+                bat.Draw();
                 pinata.Draw();
             }
         }
@@ -58,6 +58,11 @@ namespace Pinata_Game_WPF
             if (e.Key == Key.P)
             {
                 PauseGame();
+            }
+
+            if (e.Key == Key.Space)
+            {
+                bat.SwingBat();
             }
         }
 
