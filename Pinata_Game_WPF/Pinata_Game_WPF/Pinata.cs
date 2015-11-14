2c5579847281730da7637e6439ccfa6ca052d6a8
CaptainJuneBug <mikeblacknes@gmail.com>
2015-11-14 13:17:12 -0600

    Added Some Properties so you can actually use the pinata class
    Check out these properties from the Pinata Class that you can access
    from the Pinata parameter
    
    -- MyEllipse
    -- EllipseCenterPoint
    -- MyLine

--- Pinata_Game_WPF/Pinata_Game_WPF/Pinata.cs
+++ Pinata_Game_WPF/Pinata_Game_WPF/Pinata.cs
@@ -3,6 +3,7 @@
 using System;
 using System.Windows;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
@@ -11,8 +12,8 @@ namespace Pinata_Game_WPF
     internal class Pinata
     {
         private const int MAX_ANGLE = 75;
-        private const double NORMAL_INCREMENT_ANGLE = Math.PI / 3.2;
-        private const double FAST_INCREMENT_ANGLE = Math.PI / 1.7;
+        private const double NORMAL_INCREMENT_ANGLE = Math.PI / 3;
+        private const double FAST_INCREMENT_ANGLE = NORMAL_INCREMENT_ANGLE * Math.PI;
         private const string PATH = "../../images/";
         private const string EXTENSION = ".png";
 
@@ -29,6 +30,24 @@ namespace Pinata_Game_WPF
         public int NumberOfHits
         {
             get { return numberOfHits; }
+        }
+
+        public Ellipse MyEllipse
+        {
+            get { return myEllipse; }
+        }
+
+        /// <summary>
+        /// The center point of the Ellipse
+        /// </summary>
+        public Point EllipseCenterPoint
+        {
+            get { return new Point(myEllipse.Width / 2, myEllipse.Height / 2); }
+        }
+
+        public Line MyLine
+        {
+            get { return myLine; }
         }
 
         public Pinata(MainWindow window)
@@ -66,28 +85,29 @@ namespace Pinata_Game_WPF
         public void Draw()
         {
             // If the direction is going right then subtract the incrementAngle from currentAngle.
-            if (goingRight)
+            if (!goingRight)
             {
-                myLine.RenderTransform = new RotateTransform(currentAngle -= incrementAngle, myLine.X1, myLine.Y1);
+                myLine.RenderTransform = new RotateTransform(currentAngle += incrementAngle, myLine.X1, myLine.Y1);
             }
             // If the direction is going left then subtract the incrementAngle from currentAngle.
             else
             {
-                myLine.RenderTransform = new RotateTransform(currentAngle += incrementAngle, myLine.X1, myLine.Y1);
+                myLine.RenderTransform = new RotateTransform(currentAngle -= incrementAngle, myLine.X1, myLine.Y1);
             }
 
             // Creates a RotateTransform object based of the myLine.Render transform object.
             RotateTransform rotation = myLine.RenderTransform as RotateTransform;
 
             // Create two point objects p1 = beginning point of a line, while, p2 = end point of a line.
+
             Point p1 = rotation.Transform(new Point(myLine.X1, myLine.Y1));
             Point p2 = rotation.Transform(new Point(myLine.X2, myLine.Y2));
-            Console.WriteLine(p1);
 
             // Sets the center point of the ellipse to the p2 (end point of line).
-            myEllipse.Margin = new Thickness(p2.X - (myEllipse.Width / 2), p2.Y - (myEllipse.Height / 2), 0, 0);
 
+            myEllipse.Margin = new Thickness(p2.X - (myEllipse.Width / 2), p2.Y - (myEllipse.Height / 2), 0, 0);
             // If the currentAngle is less than or equal to the negative MAX_ANGLE
+
             if (currentAngle <= -MAX_ANGLE && !recentlyHit)
             {
                 goingRight = false;
@@ -130,7 +150,7 @@ namespace Pinata_Game_WPF
         {
             ImageBrush imgBrush = new ImageBrush(new BitmapImage(new Uri(PATH + "image" + (numberOfHits + 1) + EXTENSION, UriKind.Relative)));
 
-            imgBrush.Stretch = Stretch.UniformToFill;
+            //imgBrush.Stretch = Stretch.UniformToFill;
             myEllipse.Fill = imgBrush;
         }
     }
